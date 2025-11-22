5801ff00b28e6128022d711f137fa0b45db01174
Selim Memduhoğlu <selim.memduhoglu@netix.com.tr>
2025-11-22 13:35:10 +0300

    fix: Babel yapılandırması sadeleştirildi - jsxImportSource kaldırıldı
    - jsxImportSource kaldırıldı (NativeWind 4 ile uyumsuzluk)
    - Sadece nativewind/babel plugin'i kullanılıyor
    - babel-preset-expo varsayılan ayarlarla kullanılıyor

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-    ],
-    plugins: [
-      "nativewind/babel",
-    ],
+    presets: ["babel-preset-expo"],
+    plugins: ["nativewind/babel"],
   };
 };
 
